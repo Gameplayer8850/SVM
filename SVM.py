7f575c1edcc054f160f5168ddfb35373a69a02c6
Marek Żubrycki <zubrycki.marek@gmail.com>
2023-05-13 21:42:32 +0200

    Print confusion matrix with labels

--- SVM.py
+++ SVM.py
@@ -1,12 +1,22 @@
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
+import pandas as pd
 
 
 class SVM:
     def __init__(self, arguments, decisions, size, random):
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(arguments, decisions, test_size=size,
                                                                                 random_state=random)
+
+    def cm_print(self, cm, labels):
+        df = pd.DataFrame()
+        for i, row_label in enumerate(labels):
+            rowdata = {}
+            for j, col_label in enumerate(labels):
+                rowdata[col_label] = cm[i, j]
+            df = df._append(pd.DataFrame.from_dict({row_label: rowdata}, orient='index'))
+        return df[labels]
 
     def svm_algorithm(self, kernel: str, c: int):
         classifier = SVC(kernel=kernel, C=c, decision_function_shape='ovr')
@@ -18,6 +28,6 @@ class SVM:
         print("Classification accuracy: ", "{:.2f}".format(accuracy))
 
         # macierz pomyłek
-        cm = confusion_matrix(self.y_test, y_prediction)
-        print(f"Confusion matrix: \r\n{cm}")
-        # print(cm)
+        cm = confusion_matrix(self.y_test, y_prediction, labels=classifier.classes_)
+
+        print(self.cm_print(cm, classifier.classes_))
